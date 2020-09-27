30338214bab0a6c82379907807d7dfa2dfce12a0
Sebastián <jose.matamala@mail.udp.cl>
2020-09-27 03:39:44 -0300

    Se crean los modulos para poder agregar anuncios teniendo interfaz diferente entre admin y residente

--- tower1/blog/urls.py
+++ tower1/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index, home, logout, news
+from .views import index, home, logout, news, news2, anuncio
 
 
 urlpatterns = [
     path('', index, name="index"),
     path("home/", home, name="home"),
     path("logout", logout),
-    path("news/", news, name="news")
+    path("news/", news, name="news"),
+    path("news2/", news2, name="news2"),
+    path("anuncio/", anuncio, name="anuncio"),
 ]
--- tower1/blog/views.py
+++ tower1/blog/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Anuncio
+from django.contrib import messages
 
 # Create your views here.
 
@@ -54,10 +55,80 @@ def home(request):
 def news(request):
     if request.user.is_authenticated:
 
-        anuncios_admin = Anuncio.objects.filter( tipo_usuario = "1" )[:20]
-        anuncios_admin2 = Anuncio.objects.filter( tipo_usuario = "1" )[21:]
+        anuncios_admin = Anuncio.objects.filter( tipo_usuario = "1" )[:10]
+        anuncios_admin2 = Anuncio.objects.filter( tipo_usuario = "1" )[11:]
 
         return render(request,'blog/news.html',{'anuncios_admin':anuncios_admin,'anuncios_admin2':anuncios_admin2})
 
 
     return redirect('index')
+
+def news2(request):
+    if request.user.is_authenticated :
+        
+        anuncios_usuarios = Anuncio.objects.filter( tipo_usuario = "0" )[:10]
+        anuncios_usuarios2 = Anuncio.objects.filter( tipo_usuario = "0" )[11:]
+    
+        return render( request, 'blog/news2.html', {'anuncios_usuarios':anuncios_usuarios, 'anuncios_usuarios2':anuncios_usuarios2})
+    
+    return redirect('index')
+
+def anuncio(request):
+    ############ANUNCIO DE LA ADMINISTRACION###################3
+    if request.user.is_authenticated and request.user.is_staff == 1:
+        if request.POST:
+            anuncio = Anuncio()
+
+            anuncio.autor = request.user.username
+            anuncio.titulo = request.POST.get('titulo')
+            anuncio.contenido = request.POST.get('mensaje')
+            anuncio.categoria = "null"
+            anuncio.tipo_usuario = 1
+
+            priori = request.POST.get('Prioridad')
+            if priori == "Alta":
+                anuncio.prioridad = 3
+            if priori == "Media":
+                anuncio.prioridad = 2
+            if priori == "Baja":
+                anuncio.prioridad = 1                
+
+
+            try:
+                anuncio.save()
+                mensaje = "Enviado"
+                messages.success(request, mensaje)
+                return redirect('home')
+            except:
+                mensaje = "Error"
+                messages.error(request, mensaje)
+
+        return render(request,'blog/anuncio.html')
+
+    #########ANUNCIO DE LA COMUNIDAD############################3
+    if request.user.is_authenticated and request.user.is_staff == 0:
+            anuncio = Anuncio()
+
+            anuncio.autor = request.user.username
+            anuncio.titulo = request.POST.get('titulo')
+            anuncio.contenido = request.POST.get('mensaje')
+            anuncio.categoria = request.POST.get('categoria')
+            anuncio.tipo_usuario = 0
+            anuncio.prioridad = 0
+              
+
+
+            try:
+                anuncio.save()
+                mensaje = "Enviado"
+                messages.success(request, mensaje)
+                return redirect('home')
+            except:
+                mensaje = "Error"
+                messages.error(request, mensaje)
+            
+            return render(request,'blog/anuncio2.html')
+
+    return redirect('index')
+
+
